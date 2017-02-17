f7a5556416f59c075dcbfdaca22aeb354b1d81a7
manumanmax <manua380@hotmail.fr>
2017-02-17 23:21:57 +0100

    Constructor

--- IUpdateU/src/IUpdateU/link/WebpageContentFetcher.java
+++ IUpdateU/src/IUpdateU/link/WebpageContentFetcher.java
@@ -1,12 +1,25 @@
 package IUpdateU.link;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 public class WebpageContentFetcher {
 	private String pageAddress;
+	private String currentContentXLM = "";
+	private String currentContentText = "";
+	private WebClient webClient = new WebClient();
 
-	public WebpageContentFetcher(String pageAddress) {
+	public WebpageContentFetcher(String pageAddress)
+			throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		super();
 		this.pageAddress = pageAddress;
+		final HtmlPage page = webClient.getPage(pageAddress);
+		currentContentXLM = page.asXml();
+		currentContentText = page.asText();
 	}
-	
-	
+
 }
