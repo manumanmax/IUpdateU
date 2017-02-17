c826addf5226836f801f64a5c22c929dde0a8b6d
manumanmax <manua380@hotmail.fr>
2017-02-17 23:21:39 +0100

    test functional v1

--- IUpdateU/src/IUpdateU/tests/FetchContentTests.java
+++ IUpdateU/src/IUpdateU/tests/FetchContentTests.java
@@ -1,14 +1,31 @@
 package IUpdateU.tests;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class FetchContentTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	private WebClient webClient;
 
+	@Test
+	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		webClient = new WebClient();
+		final HtmlPage page = webClient.getPage("https://funforfox.blogspot.de/");
+
+		Assert.assertEquals("Body and mind", page.getTitleText());
+
+		final String pageAsXml = page.asXml();
+		System.out.println(pageAsXml);
+		Assert.assertTrue("Change in the body tag", pageAsXml.contains("<body class=\"loading variant-studio\">"));
+
+		final String pageAsText = page.asText();
+		Assert.assertTrue(pageAsText.contains("Support for the HTTP and HTTPS protocols"));
+	}
 }
