c565a768748be8e79b44ecbdd6c745448efcde50
manumanmax <manua380@hotmail.fr>
2017-02-17 23:26:57 +0100

    rename

--- IUpdateU/src/IUpdateU/link/WebpageContentContainer.java
+++ IUpdateU/src/IUpdateU/link/WebpageContentContainer.java
@@ -2,24 +2,31 @@ package IUpdateU.link;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class WebpageContentFetcher {
+public class WebpageContentContainer {
 	private String pageAddress;
 	private String currentContentXLM = "";
 	private String currentContentText = "";
 	private WebClient webClient = new WebClient();
+	ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
-	public WebpageContentFetcher(String pageAddress)
+	public WebpageContentContainer(String pageAddress)
 			throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		super();
 		this.pageAddress = pageAddress;
 		final HtmlPage page = webClient.getPage(pageAddress);
 		currentContentXLM = page.asXml();
 		currentContentText = page.asText();
+	}
+
+	public void startFetchPage() {
+
 	}
 
 }
