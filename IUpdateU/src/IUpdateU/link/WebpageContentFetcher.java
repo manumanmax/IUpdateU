6d6cc8a031aba7daed6ddbba1481fd290b8349f5
manumanmax <manua380@hotmail.fr>
2017-02-17 22:30:41 +0100

    Adding html util library

--- IUpdateU/src/IUpdateU/link/WebpageContentFetcher.java
+++ IUpdateU/src/IUpdateU/link/WebpageContentFetcher.java
@@ -1,5 +1,12 @@
 package IUpdateU.link;
 
 public class WebpageContentFetcher {
+	private String pageAddress;
 
+	public WebpageContentFetcher(String pageAddress) {
+		super();
+		this.pageAddress = pageAddress;
+	}
+	
+	
 }
