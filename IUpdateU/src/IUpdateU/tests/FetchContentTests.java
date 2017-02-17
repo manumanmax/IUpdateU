1b534ba80ba2d14df87cacd059dde9421d627a2b
manumanmax <manua380@hotmail.fr>
2017-02-17 22:00:06 +0100

    Init JUnit tests

---
+++ IUpdateU/src/IUpdateU/tests/FetchContentTests.java
@@ -0,0 +1,14 @@
+package IUpdateU.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FetchContentTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
