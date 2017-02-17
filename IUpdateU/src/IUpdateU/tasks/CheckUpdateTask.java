13970c8cfb6a3dae72e8f08631e86d11fbe0ff8d
manumanmax <manua380@hotmail.fr>
2017-02-17 23:27:17 +0100

    Task class

---
+++ IUpdateU/src/IUpdateU/tasks/CheckUpdateTask.java
@@ -0,0 +1,14 @@
+package IUpdateU.tasks;
+
+import IUpdateU.link.WebpageContentContainer;
+
+public class CheckUpdateTask implements Runnable {
+	WebpageContentContainer container;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
